828c0bc9a2300f78875d08cdf966b0377c9f2a16
Jarrett <1206905682@qq.com>
2021-05-27 10:21:50 +0800

    fix: 修改路由

--- app/router.js
+++ app/router.js
@@ -6,31 +6,50 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   // userAccess
-  router.post('/iAccounts/user/login', controller.userAccess.login);
-  router.post('/iAccounts/user/register', controller.userAccess.register);
+  router.post('/iAccounts/api/v1/user/login', controller.userAccess.login);
+  router.post(
+    '/iAccounts/api/v1/user/register',
+    controller.userAccess.register
+  );
 
   //bill
-  router.get('/iAccounts/bills/list', app.jwt, controller.bill.getBillList);
   router.get(
-    '/iAccounts/bills/balance',
+    '/iAccounts/api/v1/bills/list',
+    app.jwt,
+    controller.bill.getBillList
+  );
+  router.get(
+    '/iAccounts/api/v1/bills/balance',
     app.jwt,
     controller.bill.getBillBalance
   );
-  router.post('/iAccounts/bills', app.jwt, controller.bill.addBill);
-  router.get('/iAccounts/bills/detail', app.jwt, controller.bill.findBill);
-  router.delete('/iAccounts/bills', app.jwt, controller.bill.deleteBill);
-  router.put('/iAccounts/bills', app.jwt, controller.bill.updateBill);
+  router.post('/iAccounts/api/v1/bills', app.jwt, controller.bill.addBill);
   router.get(
-    '/iAccounts/bills/monthList',
+    '/iAccounts/api/v1/bills/detail',
+    app.jwt,
+    controller.bill.findBill
+  );
+  router.delete('/iAccounts/api/v1/bills', app.jwt, controller.bill.deleteBill);
+  router.put('/iAccounts/api/v1/bills', app.jwt, controller.bill.updateBill);
+  router.get(
+    '/iAccounts/api/v1/bills/monthList',
     app.jwt,
     controller.bill.getMonthList
   );
-  router.get('/iAccounts/bills/timeSlot', app.jwt, controller.bill.getTimeSlot);
+  router.get(
+    '/iAccounts/api/v1/bills/timeSlot',
+    app.jwt,
+    controller.bill.getTimeSlot
+  );
 
   //tags
-  router.get('/iAccounts/tags/list', app.jwt, controller.tag.getTagsByType);
-  router.get('/iAccounts/tags/detail', app.jwt, controller.tag.findTag);
-  router.post('/iAccounts/tags', app.jwt, controller.tag.addTag);
-  router.delete('/iAccounts/tags', app.jwt, controller.tag.deleteTag);
-  router.put('/iAccounts/tags', app.jwt, controller.tag.updateTag);
+  router.get(
+    '/iAccounts/api/v1/tags/list',
+    app.jwt,
+    controller.tag.getTagsByType
+  );
+  router.get('/iAccounts/api/v1/tags/detail', app.jwt, controller.tag.findTag);
+  router.post('/iAccounts/api/v1/tags', app.jwt, controller.tag.addTag);
+  router.delete('/iAccounts/api/v1/tags', app.jwt, controller.tag.deleteTag);
+  router.put('/iAccounts/api/v1/tags', app.jwt, controller.tag.updateTag);
 };
